a4f910d8d789d40acf717a91652bff602d39e017
Danel <danel.rivas@ikasle.egibide.org>
2025-02-10 10:01:00 +0100

    AÑADIDO EN MENU-MEDINA

--- src/MAIN/Main.java
+++ src/MAIN/Main.java
@@ -1,5 +1,9 @@
 package MAIN;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
