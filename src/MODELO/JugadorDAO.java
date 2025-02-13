c35c3bbb703a5029204ea24cb8f4f5a257c5fe7f
Imanol <imanol.manero@ikasle.egibide.org>
2025-02-13 14:11:16 +0100

    equipo

--- src/MODELO/JugadorDAO.java
+++ src/MODELO/JugadorDAO.java
@@ -1,5 +1,6 @@
 package MODELO;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,5 +9,50 @@ public class JugadorDAO {
 
     public JugadorDAO() {
         this.listajugadores = new ArrayList<>();
+    }
+
+    public boolean agregarJugador(Jugador jugador) {
+        if (buscarjugador(jugador.getIdJugador()) ==null ){
+            listajugadores.add(jugador);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean eliminarJugador(String idJugador) {
+        Jugador jugador = buscarjugador(idJugador);
+        if (jugador != null) {
+            listajugadores.remove(jugador);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean modificarJugador( String idJugador, String nuevoNombre, String nuevoApellido, String nuevaNacionalidad, LocalDate nuevaFechaNac, double nuevoSueldo, Equipo equipo, Roles nuevoRol) {
+        Jugador jugador = buscarjugador(idJugador);
+        if (jugador != null) {
+            if ()
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public Jugador buscarjugador(String idJugador) {
+        for (Jugador jugador : listajugadores) {
+            if (jugador.getIdJugador().equals(idJugador)){
+                return jugador;
+            }
+        }
+        return null;
     }
 }
