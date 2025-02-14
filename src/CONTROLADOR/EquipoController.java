7e5dd47194d5a6be094e1cf6380a4cebfc332c51
Unax <unax.iriondo@ikasle.egibide.org>
2025-02-14 09:30:24 +0100

    JornadaController y JornadaDAO terminados

--- src/CONTROLADOR/EquipoController.java
+++ src/CONTROLADOR/EquipoController.java
@@ -11,6 +11,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class EquipoController {
